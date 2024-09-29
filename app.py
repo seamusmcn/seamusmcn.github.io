6006120f87f10bffdc2a4d733a35692e11979aff
seamusmcn <109866486+seamusmcn@users.noreply.github.com>
2024-09-29 16:03:35 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, jsonify
 from flask_session import Session
 from flask_cors import CORS
 import requests
